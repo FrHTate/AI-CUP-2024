9d4b9876c6b8f78f6c01ea82e0aac67506ccf609
Arya0309 <arya59049@gapp.nthu.edu.tw>
2024-11-06 17:21:15 +0800

    update a

--- code/main.py
+++ code/main.py
@@ -11,9 +11,8 @@ finance_summary_path = (
 finance_path = "/home/S113062628/project/AI-CUP-2024/reference/finance_artificial.json"
 finance_old_path = "/home/S113062628/project/AI-CUP-2024/backup/finance copy.json"
 finance_ocr_path = "/home/S113062628/project/AI-CUP-2024/reference/ocr_finance.json"
-finance_temp = "/home/S113062628/project/AI-CUP-2024/temp.json"
 faq_path = "/home/S113062628/project/AI-CUP-2024/reference/faq/pid_map_content.json"
 
 jina_retrieve(
-    insurance_path, finance_temp, faq_path, chunk_size_f=256, overlap_f=32, topk=5
+    insurance_path, finance_path, faq_path, chunk_size_f=256, overlap_f=32, topk=1
 )
