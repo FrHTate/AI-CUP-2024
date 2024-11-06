64dbf1b58c2c63d0bdde877cc909d04163e2f96d
FrHTate <liquidity95171@gmail.com>
2024-11-06 17:26:48 +0800

    update .gitignore

--- code/main.py
+++ code/main.py
@@ -1,25 +1,29 @@
-from helper_jina import jina_retrieve, jina_cross_encoder
-import csv
-import json
-import re
+# Modules from pip
 import pandas as pd
+import os
 
-finance_summary_path = (
-    "/home/S113062628/project/AI-CUP-2024/reference/finance_summary.json"
-)
-finance_path = "/home/S113062628/project/AI-CUP-2024/reference/finance_artificial.json"
-finance_old_path = "/home/S113062628/project/AI-CUP-2024/backup/finance copy.json"
-finance_ocr_path = "/home/S113062628/project/AI-CUP-2024/reference/ocr_finance.json"
-finance_temp = "/home/S113062628/project/AI-CUP-2024/temp.json"
-insurance_path = "/home/S113062628/project/AI-CUP-2024/reference/ocr_insurance.json"
-faq_path = "/home/S113062628/project/AI-CUP-2024/reference/faq/pid_map_content.json"
+# Our modules
+from helper_jina import jina_retrieve, jina_cross_encoder
 
-jina_retrieve(
-    insurance_path,
-    finance_temp,
-    faq_path,
-    chunk_size_f=256,
-    overlap_f=32,
-    topk=4,
-    focus_on_source=False,
-)
+# Define paths
+path_to_folders = "/home/S112062627/Code/AI-CUP-2024"
+paths = {
+    "insurance": os.path.join(path_to_folders, "reference/insurance.json"),
+    "finance": os.path.join(path_to_folders, "reference/finance_artificial.json"),
+    "faq": os.path.join(path_to_folders, "reference/faq/pid_map_context.json"),
+    "question": os.path.join(
+        path_to_folders, "dataset/preliminary/questions_example.json"
+    ),
+    "ground_truth": os.path.join(
+        path_to_folders, "dataset/preliminary/ground_truth_example.json"
+    ),
+}
+
+# Load data
+
+
+# Retrieve top-5 by BM25
+
+# Retreive top-5 by Jina-reranker (rerank provided sources)
+
+# Reranking
