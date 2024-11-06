a8f8c2ac30e1674bc643b2e009b6d2c93b4e97b5
FrHTate <liquidity95171@gmail.com>
2024-11-06 18:02:34 +0800

    stop tracking retrievers.py

--- code/retrievers.py
+++
@@ -1,9 +0,0 @@
-def load_corpus_to_df(source_path):
-    # attributes in df = ["id", "text"]
-    id = []
-    text = []
-    
-    with open(source_path, "r") as f:
-        data = json.load(f)
-
-    if 
