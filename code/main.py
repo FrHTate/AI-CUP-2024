a1e7a189ad5abf8ee7d4e8a882d3a6e40f0eb1dc
FrHTate <liquidity95171@gmail.com>
2024-11-07 22:29:26 +0800

    update main.py

--- code/main.py
+++ code/main.py
@@ -8,11 +8,7 @@ import data_preprocesser as dp
 import retrievers as rt
 from helper_jina import jina_retrieve
 
-# from helper_jina import jina_retrieve, jina_cross_encoder
-
-# Download utils for tokenization
-
-# Define paths
+# 1. Define paths
 path_to_folders = "/home/S112062627/Code/AI-CUP-2024"
 paths = {
     "insurance": os.path.join(path_to_folders, "reference/insurance.json"),
@@ -24,11 +20,11 @@ paths = {
     "output": os.path.join(path_to_folders, "dataset/preliminary/pred_retrieve.json"),
 }
 
-# Hyperparameters
+# 2. Set Hyperparameters
 chunk_insurance = {"activate": True, "chunk_size": 128, "overlap_size": 32}
 chunk_finance = {"activate": True, "chunk_size": 256, "overlap_size": 32}
 
-# Load (and preprocessed) data
+# 3. Load data
 # Attributes: ["id": int, "text": str]
 # df_insurance_full = dp.load_corpus_to_df("insurance", paths["insurance"])
 # df_finance_full = dp.load_corpus_to_df("finance", paths["finance"])
@@ -48,8 +44,8 @@ df_faq = dp.load_corpus_to_df("faq", paths["faq"])
 # Each item: {"qid": int, "source": list of str, "query": str, "category": str}
 queries = dp.load_queries(paths["query"])
 
-# Retreive top-5 by Jina-reranker (rerank provided sources)
+# 4. Retreive top-5 by Jina-reranker (rerank provided sources)
 
-# Retrieve top-5 by BM25
+# 5. Retrieve top-5 by BM25
 
-# Hybrid
+# 6. Hybrid
